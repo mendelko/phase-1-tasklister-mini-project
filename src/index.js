e125b3395c8dd02d9c458eb1036a2bef35efc52f
Mendel <mkonikov123@gmail.com>
2021-05-23 18:56:11 -0400

    prevents default

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
 
   const createTaskForm = document.getElementById("create-task-form");
-
+  
+  
   createTaskForm.addEventListener("submit", (e) => {
     e.preventDefault();
+    const tasks = document.getElementById("tasks");
+    const li = document.createElement('li');
+    const newTaskDescription = document.getElementById('new-task-description')
+    tasks.appendChild(li);
+    li.innerHTML = newTaskDescription.value;
+
+    
   })
-});
+})
